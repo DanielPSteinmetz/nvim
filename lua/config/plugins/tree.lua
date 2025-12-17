839154248758b9c978307477f57ce85e2044c48a
Daniel Steinmetz <charamomochi@gmail.com>
2025-12-17 10:29:29 -0600

    Add commentary and filter dot files in file tree

--- lua/config/plugins/tree.lua
+++ lua/config/plugins/tree.lua
@@ -5,6 +5,7 @@ return {
       sort = { sorter = "case_sensitive" },
       view = { width = 30 },
       renderer = { group_empty = true },
+      filters = { dotfiles = true },
     })
 
     vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file tree' })
